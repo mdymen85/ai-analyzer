265a43125dec66f61f26182d9d6292f043f26af1
Martin <martin.dymenstein@matera.com>
2026-02-06 09:17:02 -0300

    init

---
+++ src/main/java/com/ai_study_group/ia_analyzer/IaAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ai_study_group.ia_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai_study_group/ia_analyzer/IaAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_study_group.ia_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
