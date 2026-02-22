16d42b7306b0b5afa4b4997a640e0ddf424ea6f4
Martin <martin.dymenstein@matera.com>
2026-02-22 14:31:37 -0300

    to other note

--- src/main/java/com/ai_study_group/ia_analyzer/controller/AIController.java
+++ src/main/java/com/ai_study_group/ia_analyzer/controller/AIController.java
@@ -1,26 +1,30 @@
-package com.ai_study_group.ia_analyzer.controller;
-
-import com.ai_study_group.ia_analyzer.service.WeatherService;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AIController {
-
-    private final ChatClient chatClient;
-
-    public AIController(ChatClient.Builder builder, WeatherService weatherService) {
-        this.chatClient = builder
-            .defaultTools(weatherService) // Register all @Tool methods in this bean
-            .build();
-    }
-
-    @GetMapping("/ask")
-    public String ask(@RequestParam String message) {
-        return chatClient.prompt(message)
-                         .call()
-                         .content();
-    }
-}
+//package com.ai_study_group.ia_analyzer.controller;
+//
+//import com.ai_study_group.ia_analyzer.service.WeatherService;
+//import com.google.genai.Client;
+//import com.google.genai.types.GenerateContentResponse;
+//import com.google.genai.types.Tool;
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class AIController {
+//
+//    private final ChatClient chatClient;
+//
+//    public AIController(ChatClient.Builder builder, WeatherService weatherService) {
+//        this.chatClient = builder
+//            .defaultTools(weatherService) // Register all @Tool methods in this bean
+//            .build();
+//    }
+//
+//    @GetMapping("/ask")
+//    public String ask(@RequestParam String message) {
+//
+//        return chatClient.prompt(message)
+//                         .call()
+//                         .content();
+//    }
+//}
